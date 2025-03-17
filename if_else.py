0458ef92506d2f2ead7823bd9469e254cc54f171
gomotencjc025 <gomotencjc025@student.wethinkcode.co.za>
2025-03-17 10:11:31 +0200

    even/odd

---
+++ if_else.py
@@ -0,0 +1,19 @@
+def even_odd(n):
+    
+    if n % 2 != 0:
+        print("Weird")
+    
+    if n % 2 == 0 and 2<= n <=5:
+        print("Not Weird")
+    
+    if n % 2 == 0 and 6<= n <=20:
+        print("Weird")
+        
+    if n % 2 == 0 and n > 20:
+        print("Not Weird")
+    
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    even_odd(n)
